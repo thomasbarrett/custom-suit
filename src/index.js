b16e2cf861a50f57866d5ff9e67e8945c7d5d1e3
Thomas Barrett <thomasbarrett@thomass-mbp.lan>
2019-09-22 18:11:03 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import * as THREE from '../node_modules/three/src/Three.js';
-import { OBJLoader } from '../node_modules/three/examples/jsm/loaders/OBJLoader.js';
-import { MTLLoader } from '../node_modules/three/examples/jsm/loaders/MTLLoader.js';
+import * as THREE from '../three/src/Three.js';
+import { OBJLoader } from '../three/examples/jsm/loaders/OBJLoader.js';
+import { MTLLoader } from '../three/examples/jsm/loaders/MTLLoader.js';
 
 console.log(THREE);
 
