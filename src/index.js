e4096d297cd6b709235cff52327ca2ef1a2cddb7
Thomas Barrett <thomasbarrett@thomass-mbp.lan>
2019-09-23 00:40:28 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -2,29 +2,56 @@ import * as THREE from '../three/src/Three.js';
 import { OBJLoader } from '../three/examples/jsm/loaders/OBJLoader.js';
 import { MTLLoader } from '../three/examples/jsm/loaders/MTLLoader.js';
 
-console.log(THREE);
+class ModelSubscribtion {
+    constructor() {
+        this.subscribers = {}
+    }
 
-const material = new THREE.MeshPhongMaterial({
-color: 0xFF0000,    // red (can also use a CSS color string here)
-flatShading: true,
-});
+    publish(event, data) {
+        console.log(this.subscribers);
+        if (!this.subscribers[event]) return;
+        this.subscribers[event].forEach(subscriberCallback =>
+            subscriberCallback(data));
+    }
+
+    subscribe(event, callback) {
+        let index;
+        if (!this.subscribers[event]) {
+            this.subscribers[event] = [];
+        }
+        index = this.subscribers[event].push(callback) - 1;
+        
+        return {
+            unsubscribe() {
+                this.subscribers[event].splice(index, 1);
+            }
+        };
+    }
+}
+
+const modelNotifier = new ModelSubscribtion();
+
 
 var materials;
 
 var container;
 var camera, scene, renderer;
-var mouseX = 0, mouseY = 0;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 let obj = null;
-var projector, mouse = {
-    x: 0,
-    y: 0
-},
-INTERSECTED;
+
+let mouse = null;
+let rotation_z = 0;
+let mouse_down = false;
 
 init();
 animate();
+
+const Suit = {
+    innerLining: ['Mat_57', 'Mat_58', 'Mat_59', 'Mat_60', 'Mat_61', 'Mat_62'],
+    upperStraps: ['Mat_10', 'Mat_16']
+};
+
 function init() {
     container = document.createElement( 'div' );
     document.body.appendChild( container );
@@ -33,21 +60,12 @@ function init() {
     // scene
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 0xffffff );
-    var ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
+    var ambientLight = new THREE.AmbientLight( 0xcccccc);
     scene.add( ambientLight );
     var pointLight = new THREE.PointLight( 0xffffff, 0.8 );
     camera.add( pointLight );
     scene.add( camera );
     
-    // model
-    var onProgress = function ( xhr ) {
-        if ( xhr.lengthComputable ) {
-            var percentComplete = xhr.loaded / xhr.total * 100;
-            console.log( Math.round( percentComplete, 2 ) + '% downloaded' );
-        }
-    };
-    var onError = function () { };
-
     let path = '819004_ENDURANCE_SUPER_PRO_Colorway_1';
     let objPath = path + '/819004_ENDURANCE_SUPER_PRO_Colorway_1.obj';
     let mtlPath = path + '/819004_ENDURANCE_SUPER_PRO_Colorway_1.mtl';
@@ -57,6 +75,13 @@ function init() {
     
         materials_.preload();
         materials = materials_;
+        materials.materials.Mat_9.transparent = true;
+        let names = ['fab_8','fab_9','fab_10','fab_14','fab_17','fab_18','fab_19','fab_21','fab_22'];
+        names.forEach(name => {
+            let material = materials.materials[name];
+            material.transparent = true;
+        });
+
         console.log(materials);
         let objLoader = new OBJLoader();
         objLoader.setMaterials(materials);
@@ -70,6 +95,9 @@ function init() {
                     child.position.x = -center.x;
                     child.position.y = -center.y;
                     child.position.z = -center.z;
+                    if (child.material == materials_.materials.fab_7) {
+                        console.log(child);
+                    }
                 })
                 object.rotation.x = 1.57;
                 scene.add(object);
@@ -86,6 +114,69 @@ function init() {
     renderer.setSize( window.innerWidth / 2,  window.innerHeight );
     container.appendChild( renderer.domElement );
     //
+    container.addEventListener('click', onDocumentMouseDown );
+
+    const dragStart = (event) => {
+        mouse_down = true;
+        let rect = event.target.getBoundingClientRect();
+        let x = event.clientX - rect.left; //x position within the element.
+        let y = event.clientY - rect.top;  //y position within the element.
+        mouse = {x, y};
+        rotation_z = obj.rotation.z;
+    };
+
+    const dragEnd = (event) => {
+        if (mouse_down) {
+            mouse_down = false;
+            let rect = event.target.getBoundingClientRect();
+            let x = event.clientX - rect.left; //x position within the element.
+            let y = event.clientY - rect.top;  //y position within the element.
+            let mouse2 = {x, y};
+            let dmouse = {x: mouse2.x - mouse.x, y: mouse2.y - mouse.y};
+            obj.rotation.z = rotation_z - 3 * dmouse.x / rect.width;
+        }
+    };
+
+    const dragMove = (event) => {
+        if (mouse_down) {
+            let rect = event.target.getBoundingClientRect();
+            let x = event.clientX - rect.left; //x position within the element.
+            let y = event.clientY - rect.top;  //y position within the element.
+            let mouse2 = {x, y};
+            let dmouse = {x: mouse2.x - mouse.x, y: mouse2.y - mouse.y};
+            obj.rotation.z = rotation_z - 3 * dmouse.x / rect.width;
+        }
+    }
+
+    container.addEventListener('mousedown', dragStart);
+    container.addEventListener('mouseup', dragEnd);
+    container.addEventListener('mousemove', dragMove);
+    container.addEventListener('mouseleave', dragEnd);
+
+        container.addEventListener('touchstart', (event) => {
+            mouse_down = true;
+            let rect = event.touches[0].target.getBoundingClientRect();
+            let x = event.touches[0].clientX - rect.left; //x position within the element.
+            let y = event.touches[0].clientY - rect.top;  //y position within the element.
+            mouse = {x, y};
+            rotation_z = obj.rotation.z;
+        });
+        container.addEventListener('touchmove', (event) => {
+            if (mouse_down) {
+                let rect = event.touches[0].target.getBoundingClientRect();
+                let x = event.touches[0].clientX - rect.left; //x position within the element.
+                let y = event.touches[0].clientY - rect.top;  //y position within the element.
+                let mouse2 = {x, y};
+                let dmouse = {x: mouse2.x - mouse.x, y: mouse2.y - mouse.y};
+                obj.rotation.z = rotation_z - 3 * dmouse.x / rect.width;
+            }
+        });
+
+    /*
+    container.addEventListener('touchend', dragEnd);
+    container.addEventListener('touchmove', dragMove);
+    container.addEventListener('touchcancel', dragEnd);
+    */
     window.addEventListener( 'resize', onWindowResize, false );
 }
 
@@ -97,6 +188,35 @@ function onWindowResize() {
     renderer.setSize( window.innerWidth / 2, window.innerHeight );
 }
 
+function onDocumentMouseDown( event ) {  
+    event.preventDefault();
+    let rect = event.target.getBoundingClientRect();
+    let x = event.clientX - rect.left; //x position within the element.
+    let y = event.clientY - rect.top;  //y position within the element.
+    var raycaster = new THREE.Raycaster(); 
+    raycaster.setFromCamera( {x: 2 * x / rect.width - 1, y: 1 - 2 * y / rect.height}, camera ); 
+    let intersects = raycaster.intersectObjects( obj.children ); 
+    if (intersects.length > 0) {
+        let object = intersects[0].object;
+        if (object.name === 'Mat_6') {
+            modelNotifier.publish('front-side', [object]);
+        } else if (object.name === 'Mat_13') {
+            modelNotifier.publish('back-strap', [object]);
+        } else if (object.name === 'Mat_15') {
+            modelNotifier.publish('right-side', [object]);
+        } else if (object.name === 'Mat_18') {
+            modelNotifier.publish('left-side', [object]);
+        } else if (Suit.innerLining.includes(object.name)) {
+            modelNotifier.publish('inner-lining', obj.children.filter(child => Suit.innerLining.includes(child.name)));
+        } else if (Suit.upperStraps.includes(object.name)) {
+            modelNotifier.publish('upper-straps', obj.children.filter(child => Suit.upperStraps.includes(child.name)));
+        } else {
+            console.log(object);
+        }
+    } else {
+        modelNotifier.publish('clear');
+    }
+}
 
 function animate() {
     requestAnimationFrame(animate);
@@ -105,7 +225,7 @@ function animate() {
 }
 
 function update() {
-    if (obj) obj.rotation.z += 0.005;
+   // obj.rotation.z += 0.05;
 }
 
 function render() {
@@ -113,37 +233,51 @@ function render() {
 }
 
 function stitching(foo) {
-    let names = ['fab_8','fab_9','fab_10','fab_14','fab_17','fab_18','fab_19','fab_21','fab_22'];
+    let names = ['fab_14','fab_17','fab_18','fab_19','fab_21','fab_22'];
     names.forEach(name => {
         let material = materials.materials[name];
         material.color.setHex(foo);
     });
 }
 
-function front(foo) {
-    materials.materials.Mat_9.map = THREE.ImageUtils.loadTexture( foo );
-    materials.materials.Mat_9.needsUpdate = true;
+function front(hex) {
+    obj.children.forEach(child => {
+        if (child.name === Suit.front) {
+            child.material = child.material.clone();
+            child.material.color.setHex(hex);
+        }
+    });
 }
 
+function left(hex) {
+    obj.children.forEach(child => {
+        if (child.name === Suit.leftSide) {
+            child.material = child.material.clone();
+            child.material.color.setHex(hex);
+        }
+    });
+}
+
+function right(hex) {
+    obj.children.forEach(child => {
+        if (child.name === Suit.rightSide) {
+            child.material = child.material.clone();
+            child.material.color.setHex(hex);
+        }
+    });
+}
 function strap(foo) {
-    materials.materials.Mat_11.color.setHex(foo);
-    materials.materials.Mat_11.emissive.setHex(foo);
-    materials.materials.Mat_11.emissiveIntensity = 0.4; 
-
-    materials.materials.Mat_13.color.setHex(foo);
-    materials.materials.Mat_13.emissive.setHex(foo);
-    materials.materials.Mat_13.emissiveIntensity = 0.4; 
-
+    materials.materials.fab_8.color.setHex(foo);
+    materials.materials.fab_9.color.setHex(foo);
+    materials.materials.fab_10.color.setHex(foo);
 }
 
 function fabric(foo) { 
    materials.materials.fab_7.color.setHex(foo);
-   materials.materials.fab_7.emissive.setHex(foo);
-   materials.materials.fab_7.emissiveIntensity = 0.4; 
 }
 
 function liner(foo) { 
     materials.materials.fab_6.color.setHex(foo);
 }
  
-export {stitching, fabric, liner, strap, front};
+export {stitching, fabric, liner, strap, front, left, right, modelNotifier};
